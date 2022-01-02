617f7e218fa62e28f9979d64cb99badf09bac246
Tomasz Budny <tbudny@fandom.com>
2022-01-02 18:28:52 +0100

    feat: blanc card

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flash Clouds'),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              width: 300.0,
+              height: 100.0,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    width: 1.0,
+                    color: Colors.blueGrey.withOpacity(0.3),
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blueGrey.withOpacity(0.3),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    )
+                  ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
