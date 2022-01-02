fc18919f59765aa276626fe59473d510e368a988
Tomasz Budny <tbudny@fandom.com>
2022-01-02 19:51:06 +0100

    feat: dummy bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() {
   runApp(const Main());
 }
 
-class Main extends StatelessWidget {
+class Main extends StatefulWidget {
   const Main({Key? key}) : super(key: key);
+
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  int _bottomNavState = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,28 +23,60 @@ class Main extends StatelessWidget {
         ),
         body: SafeArea(
           child: Center(
-            child: Container(
-              width: 300.0,
-              height: 100.0,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    width: 1.0,
-                    color: Colors.blueGrey.withOpacity(0.3),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          width: 1.0,
+                          color: Colors.blueGrey.withOpacity(0.3),
+                        ),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.blueGrey.withOpacity(0.3),
+                            spreadRadius: 3,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
+                          )
+                        ]),
                   ),
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.blueGrey.withOpacity(0.3),
-                      spreadRadius: 3,
-                      blurRadius: 5,
-                      offset: const Offset(0, 3),
-                    )
-                  ]),
+                )
+              ],
             ),
           ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.post_add_outlined),
+              label: 'Add',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.reorder),
+              label: 'Deck',
+            ),
+          ],
+          currentIndex: _bottomNavState,
+          selectedItemColor: Colors.amber[800],
+          onTap: _updateBottomNavState,
+        ),
       ),
     );
+  }
+
+  void _updateBottomNavState(int index) {
+    setState(() {
+      _bottomNavState = index;
+    });
   }
 }
