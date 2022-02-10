64f9f0345f67ebdd9431d40b9a6a70b7294a4add
Tomasz Budny <tbudny@fandom.com>
2022-02-10 23:03:40 +0100

    tests[integration]: check if All Cards View flipping works and refreshes on cards deletions from other Views

--- integration_test/tests/all_cards_view_test.dart
+++ integration_test/tests/all_cards_view_test.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+import '../elements/add_test_card.dart';
 import '../elements/bottom_navbar_finder.dart';
+import '../elements/clear_cards.dart';
+import '../elements/find_card.dart';
+import '../elements/tap_card.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Test ALL CARDS View', () {
-    testWidgets('Test ALL CARDS View - First Load',
-        (WidgetTester tester) async {
+  group('Test ALL CARDS', () {
+    testWidgets('First Load', (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
       final allCardsButton = find.byIcon(Icons.receipt_long);
@@ -22,6 +25,63 @@ void main() {
       expect(find.text(''), findsNothing);
 
       findBottomNavBar();
+    });
+
+    testWidgets(
+        'Add Two Cards, check if they are visible and flip '
+        'in All Cards View.', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      await addTestCard(tester, 'Sample Question', 'Sample Answer');
+      await goToAllCardsView(tester);
+      await addTestCard(tester, 'Sample Question 2', 'Sample Answer 2');
+      await goToAllCardsView(tester);
+
+      // Find first card
+      findCard('Sample Question', 'Sample Answer');
+      await tapCard(tester, 'Sample Question', 'Sample Answer');
+
+      // Find second card
+      findCard('Sample Question 2', 'Sample Answer 2');
+      await tapCard(tester, 'Sample Question 2', 'Sample Answer 2');
+
+      // Clean up
+      await clearCards(tester);
+    });
+
+    testWidgets(
+        'Add Two Cards, remove them in Add Cards View and then in Learn View '
+        'and check if All Cards View refreshes after each delete',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      await addTestCard(tester, 'Sample Question', 'Sample Answer');
+      await goToAllCardsView(tester);
+      await addTestCard(tester, 'Sample Question 2', 'Sample Answer 2');
+
+      // Confirm cards in All Cards View
+      await goToAllCardsView(tester);
+      findCard('Sample Question', 'Sample Answer');
+      findCard('Sample Question 2', 'Sample Answer 2');
+
+      // Delete card from Add Cards View
+      await goToAddCardsView(tester);
+      await tester.tap(find.byIcon(Icons.delete_outlined).first);
+      await tester.pumpAndSettle();
+
+      // Check if All Cards View refreshed
+      await goToAllCardsView(tester);
+      findCard('Sample Question', 'Sample Answer');
+      notFindCard('Sample Question 2', 'Sample Answer 2');
+
+      // Delete card from Learn View
+      await goToLearnView(tester);
+      await tester.tap(find.byIcon(Icons.delete_outlined).first);
+      await tester.pumpAndSettle();
+
+      // Check if All Cards View refreshed
+      await goToAllCardsView(tester);
+      notFindCard('Sample Question', 'Sample Answer');
     });
   });
 }
