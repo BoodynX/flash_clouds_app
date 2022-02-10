0eb4279565de1ef18d0b3192c6e12882db3e09c7
Tomasz Budny <tbudny@fandom.com>
2022-02-10 21:22:10 +0100

    tests[integration]: check if Add Cards View refreshes on cards deletions from other Views

--- integration_test/tests/add_cards_view_test.dart
+++ integration_test/tests/add_cards_view_test.dart
@@ -5,6 +5,7 @@ import 'package:integration_test/integration_test.dart';
 
 import '../elements/add_test_card.dart';
 import '../elements/bottom_navbar_finder.dart';
+import '../elements/clear_cards.dart';
 import '../elements/find_card.dart';
 import '../elements/tap_card.dart';
 
@@ -12,8 +13,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Test ADD CARDS View', () {
-    testWidgets('Test ADD CARDS View. First Load.',
-        (WidgetTester tester) async {
+    testWidgets('First Load.', (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
       await goToAddCardsView(tester);
@@ -36,9 +36,7 @@ void main() {
       findBottomNavBar();
     });
 
-    testWidgets(
-        'Test ADD CARDS View. Add two cards, '
-        'tap one from both sides and delete them.',
+    testWidgets('Add two cards, tap one from both sides and delete them.',
         (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
@@ -68,6 +66,59 @@ void main() {
 
       // Check last added card
       expect(find.text(''), findsNWidgets(4));
+    });
+
+    testWidgets('Check removal of last added card, from All Cards View',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      await addTestCard(tester, 'Sample Question', 'Sample Answer');
+      await addTestCard(tester, 'Sample Question 2', 'Sample Answer 2');
+
+      // Delete the last added card in the All Cards View
+      // We have to tap the one before last Trash Icon because the last one
+      // is on the back side of the card, and the tester can't tap what is not
+      // visible in the view port
+      await goToAllCardsView(tester);
+      await tester.tap(find.byIcon(Icons.delete_outlined).hitTestable().last);
+      await tester.pumpAndSettle();
+
+      // Check if the first card replaced the second in Lear View
+      await goToAddCardsView(tester);
+      findCard('Sample Question', 'Sample Answer');
+
+      // Clean up
+      await clearCards(tester);
+    });
+
+    testWidgets('Check removal of last added card, from Learn View',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      await addTestCard(tester, 'Sample Question', 'Sample Answer');
+
+      // Make sure the first card will be drawn in the Learn view
+      await goToLearnView(tester);
+      findCard('Sample Question', 'Sample Answer');
+
+      await addTestCard(tester, 'Sample Question 2', 'Sample Answer 2');
+      await goToLearnView(tester);
+
+      // Press any Familiarity button to draw the second
+      await tester.tap(find.widgetWithText(ElevatedButton, 'Perfect').first);
+      await tester.pumpAndSettle();
+      findCard('Sample Question 2', 'Sample Answer 2');
+
+      // Delete the second card from the Learn View
+      await tester.tap(find.byIcon(Icons.delete_outlined).first);
+      await tester.pumpAndSettle();
+
+      // Check if the first card replaced the second in Add Cards View
+      await goToAddCardsView(tester);
+      findCard('Sample Question', 'Sample Answer');
+
+      // Clean up
+      await clearCards(tester);
     });
   });
 }
